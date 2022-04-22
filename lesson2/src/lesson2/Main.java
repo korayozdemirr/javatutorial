9d74c7028e83bc7eb88dd042c8bcc733067f9bde
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-04-22 19:25:13 +0300

    şart blockları if else switch case

---
+++ lesson2/src/lesson2/Main.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//şart blockları (Conditionals)
+		int sayi = 20;
+		
+		if(sayi < 20) {
+			System.out.println("sayi 20'den küçüktür");
+		}else {
+			System.out.println("sayi 20'den büyük veya 20ye eşit");
+		}
+	}
+
+}
