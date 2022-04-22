1f660fa16634b922bc537ef6703d2210aee23c16
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-04-22 19:18:30 +0300

    ders 1 değişkenler

--- lesson1/src/lesson1/main.java
+++ lesson1/src/lesson1/main.java
@@ -4,7 +4,15 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello world");
+		System.out.println("lesson1");
+		//Variables
+		int studentCount = 9;
+		String studentName="Koray Özdemir";
+		boolean student = true;
+		System.out.println("Öğrenci Sayım: " + studentCount);
+		System.out.println("Öğrenci adı: " + studentName.toString());
+		System.out.println("Öğrenci kaydı: " + student);
+		
 	}
 
 }
